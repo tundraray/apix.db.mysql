d75351eaeb0e7c5cd16c5c2f8710362c3fb772f1
Mikhail Andreev <my@mihail-andreev.ru>
2017-11-01 23:47:42 +0300

    fix

--- src/Apix.Db.MySql/Properties/AssemblyInfo.cs
+++ src/Apix.Db.MySql/Properties/AssemblyInfo.cs
@@ -9,7 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Apix.Repositories.Dapper")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("2.10.*")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
