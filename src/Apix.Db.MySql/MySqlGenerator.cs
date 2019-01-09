6cf4da2716de297bd015b168a8dbc256ceecb25c
Mikhail Andreev <tundraray@outlook.com>
2019-01-09 18:44:42 +0300

    Version 1.0.14 (#2)
    * add scalar query
    
    * update version
    
    * update
    
    * bitbucket-pipelines.yml edited online with Bitbucket
    
    * fix project
    
    * fix insert
    
    * update driver
    
    * grow version
    
    * fix InsertQueryWithResult
    
    * -
    
    * fix GenerateInsertQueryWithResult
    
    * as';d
    asl';d
    
    * fix
    
    * Apix.Db.MySql.csproj edited online with Bitbucket
    
    * MySqlGenerator.cs edited online with Bitbucket
    
    * MySqlGenerator.cs edited online with Bitbucket
    
    * update reference
    
    * change net standard
    
    * Apix.Db.MySql.csproj edited online with Bitbucket
    
    * fix
    
    * fix
    
    * update core framework
    
    * bitbucket-pipelines.yml edited online with Bitbucket

--- src/Apix.Db.MySql/MySqlGenerator.cs
+++ src/Apix.Db.MySql/MySqlGenerator.cs
@@ -99,7 +99,7 @@ namespace Apix.Db.Mysql
                     columnValues.Append(",");
                 }
 
-                columnNames.Append($"{properties[i].GetDatabaseFieldName()}");
+                columnNames.Append($"`{properties[i].GetDatabaseFieldName()}`");
 
                 columnValues.Append($"@{properties[i].Name}");
 
@@ -114,8 +114,8 @@ namespace Apix.Db.Mysql
         {
             var type = typeof(T).GetTypeInfo();
             var tableName = type.GetTableName();
-            return GetQuery(type, SqlQueryType.Insert, tableName) ??
-                   AddQuery(type, SqlQueryType.Insert, tableName, GenerateInsertQueryWithResult(type, tableName));
+            return GetQuery(type, SqlQueryType.InsertWithResult, tableName) ??
+                   AddQuery(type, SqlQueryType.InsertWithResult, tableName, GenerateInsertQueryWithResult(type, tableName));
         }
 
         private static string GenerateInsertQueryWithResult(TypeInfo type, string tableName)
@@ -139,18 +139,17 @@ namespace Apix.Db.Mysql
                     columnValues.Append(",");
                 }
 
-                columnNames.Append($"{properties[i].GetDatabaseFieldName()}");
+                columnNames.Append($"`{properties[i].GetDatabaseFieldName()}`");
                 columnValues.Append($"@{properties[i].Name}");
-
-                if (conditionCounter > 0)
-                {
-                    condition.Append(" AND ");
-                }
-
+                
                 if (properties[i].IsDatabaseIdentity() || properties[i].IsDatabaseAutoIncrement())
                 {
+                    if (conditionCounter > 0)
+                    {
+                        condition.Append(" AND ");
+                    }
                     var property = (properties[i].IsDatabaseAutoIncrement()
-                        ? "SCOPE_IDENTITY()"
+                        ? "LAST_INSERT_ID()"
                         : "@" + properties[i].Name);
                     condition.Append($"`{properties[i].GetDatabaseFieldName()}` = {property}");
                     conditionCounter++;
@@ -159,7 +158,7 @@ namespace Apix.Db.Mysql
 
 
             return $"INSERT INTO {tableName} ({columnNames}) VALUES ({columnValues});" +
-                   $"{selectQuery} WHERE ";
+                   $"{selectQuery} WHERE {condition}";
         }
 
         #endregion
@@ -410,7 +409,7 @@ namespace Apix.Db.Mysql
         {
             if (body.NodeType != ExpressionType.AndAlso && body.NodeType != ExpressionType.OrElse)
             {
-                var propertyName = ((MemberExpression)body.Left).Member.GetDatabaseFieldName();
+                var propertyName = GetCorrectPropertyName(body.Left);
                 var propertyValue = Expression.Lambda(body.Right).Compile().DynamicInvoke();
                 var opr = GetOperator(body.NodeType);
                 var link = GetOperator(linkingType);
@@ -422,6 +421,17 @@ namespace Apix.Db.Mysql
                 WalkTree((BinaryExpression)body.Left, body.NodeType, ref queryProperties);
                 WalkTree((BinaryExpression)body.Right, body.NodeType, ref queryProperties);
             }
+        }
+
+        private static string GetCorrectPropertyName(Expression expression)
+        {
+            if (expression is MemberExpression) {
+                return ((MemberExpression)expression).Member.GetDatabaseFieldName();
+            }
+            else {
+                var op = ((UnaryExpression)expression).Operand;
+                return ((MemberExpression)op).Member.GetDatabaseFieldName();
+            }                
         }
         /// <summary>
         /// 
